e429f80f21ce927c034f694332d0e1609fc97fb6
DeadOrk <wybertgp@gmail.com>
2025-03-01 22:38:08 +0700

    Добавлена логика работы приложения. При нажатии на кнопку идет считывание числа из поля ввода и рассчет выражения по формуле из варианта с использованием цикла.

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.lab2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.sin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.calculateButton).setOnClickListener {
+
+            val nStr = findViewById<EditText>(R.id.nInput).text.toString()
+            if (nStr.isEmpty()) {
+                Toast.makeText(this, "Введите число!", Toast.LENGTH_SHORT).show()
+            }
+
+            val n = nStr.toIntOrNull() ?: 0
+            if (n < 1) {
+                Toast.makeText(this, "Число должно быть натуральным!", Toast.LENGTH_SHORT).show()
+            }
+
+            var Sum = 0.0
+            for (k in 1..n) {
+                var denominator = 0.0
+                for (i in 1..k) {
+                    denominator += sin(i.toDouble())
+                }
+
+                if (denominator == 0.0) {
+                    Toast.makeText(this, "Ошибка: деление на ноль", Toast.LENGTH_SHORT).show()
+                }
+
+                Sum += 1.0 / denominator
+            }
+
+            findViewById<TextView>(R.id.resultTextView).text =
+                "Результат: ${"%.4f".format(Sum)}"
+
+        }
+
     }
 }
